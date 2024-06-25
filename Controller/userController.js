17c9b68509b1cfbcf0beec373ad01b297f81fa6d
Sumayya PB <sumayyapb38@gmail.com>
2024-06-25 22:36:45 +0530

    latest changes

--- Controller/userController.js
+++ Controller/userController.js
@@ -1,139 +1,138 @@
 import USER from "../model/userModel.js";
-import  mongoose  from "mongoose";
-import bcrypt from 'bcrypt'
-import jwt from 'jsonwebtoken'
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
-const doSignUp = async(req,res)=>{
- console.log(req.body)
-   try {
-      const {firstName,lastName,email,password,city} = req.body
-    
-      const userExist = await USER.find({email})
-
-      if(!userExist){
-        return res.status(400).json({message:'User already exist'})
-         
-      }
-
-      const saltRounds = 10;
-      const hashedPassword = await bcrypt.hash(password,saltRounds)
-
-      const user = new USER({
-        firstName,
-        lastName,
-        email,
-        city,
-        password:hashedPassword,
-        role : 'user'
-        
-      })
-       await user.save()
-       console.log(user);
-      
-      if(!user){
-         return  res.status(400).json('user is not created')
-      }
-     
-      res.status(201).json({ message: "successfully signed in!"});
-    
-   } catch (error) {
-     console.log(error);
-     return res.status(500).json('internal server error')
-   }
-}
-
-const doLogin  = async (req,res)=>{
+const doSignUp = async (req, res) => {
+  console.log(req.body);
   try {
-    const {email,password}=req.body
+    const { firstName, lastName, email, password, city } = req.body;
 
-    const user = await USER.findOne({email})
+    const userExist = await USER.find({ email });
 
-    if(!user){
-      return res.status(400).json({message:"User not exist"})
+    if (!userExist) {
+      return res.status(400).json({ message: "User already exist" });
     }
-    const matchPassword = await bcrypt.compare(password , user.password)
 
-    if(!matchPassword){
-      return res.status(400).json("incorrect password")
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+    const user = new USER({
+      firstName,
+      lastName,
+      email,
+      city,
+      password: hashedPassword,
+      role: "user",
+    });
+    await user.save();
+    console.log(user);
+
+    if (!user) {
+      return res.status(400).json("user is not created");
     }
-   const payload = {id : user.id, role : user.role}
-    const token = jwt.sign(payload,process.env.SECRET_KEY,{expiresIn:"1d"})
-    res.cookie("token",token);
-    res.status(200).json({message:"Logged In!"})
-    } catch (error) {
+
+    res.status(201).json({ message: "successfully signed in!" });
+  } catch (error) {
     console.log(error);
-    return res.status(500).json('internal server error')
+    return res.status(500).json("internal server error");
   }
-}
- 
+};
+
+const doLogin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await USER.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({ message: "User not exist" });
+    }
+    const matchPassword = await bcrypt.compare(password, user.password);
+
+    if (!matchPassword) {
+      return res.status(400).json("incorrect password");
+    }
+    const payload = { id: user.id, role: user.role };
+    const token = jwt.sign(payload, process.env.SECRET_KEY, {
+      expiresIn: "1d",
+    });
+    // res.cookie("token",token);
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: true, // Set to true if you're using HTTPS
+      sameSite: "None", // Necessary for cross-site cookies
+    });
+    res.status(200).json({ message: "Logged In!" });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json("internal server error");
+  }
+};
 
 const checkLogin = async (req, res) => {
   try {
     const user = req.user;
-    console.log("role ", user.role)
+    console.log("role ", user.role);
 
     console.log("Authenticated user ID:", user.id);
 
-
     // const findUser = await USER.findById({ id: user.id });
-    const findUser = await USER.findOne({ _id: new mongoose.Types.ObjectId(user.id) });
+    const findUser = await USER.findOne({
+      _id: new mongoose.Types.ObjectId(user.id),
+    });
 
     if (!findUser) {
-      return res.status(401).json({ message: "Authentication failed", success: false });
+      return res
+        .status(401)
+        .json({ message: "Authentication failed", success: false });
     }
 
     res.json({ message: "User authenticated", success: true });
   } catch (error) {
     console.error("Internal server error:", error);
-    res.status(500).json({ message: 'Internal server error', success: false });
+    res.status(500).json({ message: "Internal server error", success: false });
   }
 };
 
-const getUser = async(req,res)=>{
+const getUser = async (req, res) => {
   try {
-    const getUser = await USER.find({})
-    if(!getUser){
-     res.status(400).json('Not found the user')
+    const getUser = await USER.find({});
+    if (!getUser) {
+      res.status(400).json("Not found the user");
     }
-    res.status(200).json(getUser)
- } catch (error) {
-  console.log(error);
-  res.status(500).json('Internal server error')
- }
-}
-const changeCity = async(req,res)=>{
+    res.status(200).json(getUser);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json("Internal server error");
+  }
+};
+const changeCity = async (req, res) => {
   try {
-    const {city}= req.body
-    const user = await USER.findOne({_id: req.userId})
+    const { city } = req.body;
+    const user = await USER.findOne({ _id: req.userId });
 
-    if(!user){
-      return res.status(400).json("Invalid Credential")
-    }else{
-      user.city = city
+    if (!user) {
+      return res.status(400).json("Invalid Credential");
+    } else {
+      user.city = city;
       await user.save();
-      return res.status(200).json({message:"city changed successfully"})
+      return res.status(200).json({ message: "city changed successfully" });
     }
-    
   } catch (error) {
     console.log(error);
-     res.status(500).json('Internal server error')
+    res.status(500).json("Internal server error");
   }
-}
+};
 
-const logOut = async(req,res)=>{
+const logOut = async (req, res) => {
   try {
-    res.clearCookie("token")
-  res.status(200).json({messgae:"user Loggout successfully"})
+    res.clearCookie("token");
+    res.status(200).json({ messgae: "user Loggout successfully" });
   } catch (error) {
     console.log(error);
-     res.status(500).json('Internal server error')
+    res.status(500).json("Internal server error");
   }
-}
+};
 
-
-
-
-
-export {doSignUp,doLogin,checkLogin,getUser,logOut,changeCity} 
-
-
+export { doSignUp, doLogin, checkLogin, getUser, logOut, changeCity };
